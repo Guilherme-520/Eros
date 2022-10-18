2fed457b16c02a97b9649aefbe8d221f98a126ab
Guilherme-520 <guilhermecordeiro520@outlook.com>
2022-10-18 15:17:05 -0300

    component radio

--- src/pages/Questions/Hair.js
+++ src/pages/Questions/Hair.js
@@ -1,10 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
+import Radio from '../../components/Radio'
 
 export default function Hair() {
   return (
     <View>
       <Text>Perguntas sobre cabelo</Text>
+      <Radio/>
     </View>
   )
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,9 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Welcome from "../pages/Welcome";
 import SignIn from "../pages/Signin";
 import SignUp from "../pages/SignUp";
-import Home from '../pages/Home'
+import Home from '../pages/Home';
+import Hair from '../pages/Questions/Hair';
+import Skins from '../pages/Questions/Skins';
 
 const Stack = createNativeStackNavigator();
 
@@ -24,6 +26,16 @@ export default function Routes(){
             <Stack.Screen
             name="SignUp"
             component={SignUp}
+            options={{ headerShown: false}}
+            />
+            <Stack.Screen
+            name="Hair"
+            component={Hair}
+            options={{ headerShown: false}}
+            />
+            <Stack.Screen
+            name="Skins"
+            component={Skins}
             options={{ headerShown: false}}
             />
             <Stack.Screen
